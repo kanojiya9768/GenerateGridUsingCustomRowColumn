9a2c61711e2f6c63ff9966e34a952eda7c8e131c
Vishal Brijesh kanojiya <90042549+kanojiya9768@users.noreply.github.com>
2023-05-28 16:44:32 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App'
-// import './index.css'
-// // import { AppProvider } from './Component/UseContext_or_Context_api_6'
-// import {AppProvider} from './Component/UseContext/useContext'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <AppProvider>
-//     <App />
-//   </AppProvider>
-
-// )
-
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
